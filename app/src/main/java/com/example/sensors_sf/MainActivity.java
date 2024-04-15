7b8326683793f92008f839910d9c64497f961651
Michael Kirss <Michaelkirss@gmail.com>
2024-04-15 20:43:15 +0400

    Отображение списка датчиков

--- app/src/main/java/com/example/sensors_sf/MainActivity.java
+++ app/src/main/java/com/example/sensors_sf/MainActivity.java
@@ -1,14 +1,22 @@
 package com.example.sensors_sf;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
 import android.os.Bundle;
-
+import android.widget.TextView;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView textView;
+    private SensorManager sensorManager;
+    private List<Sensor> deviceSensors;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +28,15 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        textView=findViewById(R.id.textView);
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        deviceSensors = sensorManager.getSensorList(Sensor.TYPE_ALL);
+
+        printSensors();
+    }
+    private void printSensors(){
+        for(Sensor sensor: deviceSensors) {
+            textView.setText(textView.getText() + "\n" + sensor.getName());
+        }
     }
 }
